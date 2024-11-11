2c081e42416ee59ed2beccb42d7806fcf8d43785
Malick Ouedraogo <C00400598@louisiana.edu>
2024-11-11 17:43:37 -0600

    Set up search page and api requests

---
+++ app/(tabs)/search/Search.style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const searchStyle = StyleSheet.create({
+    container: {
+        padding: 10,
+    },
+    searchInput: {
+        backgroundColor: 'white',
+        height: 40,
+        padding: 10,
+        borderRadius: 10
+    }
+})
